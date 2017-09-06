341943af365b0e792a8236d3b947a80432e0d832
Francois Bastardie <fba@aqua.dtu.dk>
2017-09-06 15:41:00 +0200

    Toward implementing a standalone avaifieldupdater (wip)

--- InteractiveRscripts/input2AvaiUpdater.R
+++ InteractiveRscripts/input2AvaiUpdater.R
@@ -13,9 +13,11 @@
   }
 
  fake <- data.frame(Survey=NA, Year=c(2014,2014), ShootLon=c(18.79,11.0450), ShootLat=c(55.8667,54.0875),
-                      Stock="COD.2224", StockId=10, nb_indiv.0=100, nb_indiv.1=10, nb_indiv2=1)
+                      Stock="COD.2224", StockId=10, nb_indiv.0=100, nb_indiv.1=10, nb_indiv.2=1,
+                      nb_indiv.3=1, nb_indiv.4=1, nb_indiv.5=1, nb_indiv.6=1, nb_indiv.7=1, nb_indiv.8=1, nb_indiv.9=1,
+                      nb_indiv.10=1, nb_indiv.11=1, nb_indiv.12=1, nb_indiv.13=1)  # caution: THE FORMAT IS NOT FLEXIBLE AT ALL
 
- write.table(fake, file= file.path( general$main_path,
-             "input_file_for_displace_merger.csv"), sep=";", row.names=FALSE, quote=FALSE)
+ write.table(fake, file= file.path( general$main_path, paste("popsspe_", general$application, sep=''), "static_avai",
+             "displace_input_for_data_merger.dat"), sep=" ", row.names=FALSE, quote=FALSE)   ## CAUTION file is .dat and sep is white space
 
 
